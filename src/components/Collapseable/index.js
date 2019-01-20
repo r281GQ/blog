31c6d9914631061cf340648b86fe613676dbf3ac
Endre Vegh <endre.vegh@yahoo.com>
2019-01-20 18:01:10 +0000

    add propTypes to <Collapseable />

--- src/components/Collapseable/index.js
+++ src/components/Collapseable/index.js
@@ -1,10 +1,21 @@
+import propTypes from 'prop-types'
 import styled from 'styled-components'
 
-export default styled.div`
-  visibility: collapse;
+const Collapseable = styled.div`
+  display: none;
 
   @media only screen and (min-width: ${props =>
       props.theme.breakpoints[props.collapse]}) {
-    visibility: inherit;
+    display: inherit;
   }
 `
+
+Collapseable.propTypes = {
+  collapse: propTypes.number,
+}
+
+Collapseable.defaultProps = {
+  collapse: 0,
+}
+
+export { Collapseable }
