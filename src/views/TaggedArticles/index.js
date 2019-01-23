b86d85be71c20fc7d34e4e058dafbf57f9979bb6
Endre Vegh <endre.vegh@yahoo.com>
2019-01-23 21:56:22 +0000

    add TaggedArticles

---
+++ src/views/TaggedArticles/index.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import Page from '../../containers/Page'
+
+import Layout from './../../components/Layout'
+
+import RecentArticles from './../RecentArticles'
+
+const TaggedArticles = props => (
+  <Layout>
+    <Page {...props}>
+      {({ posts }) => (
+        <RecentArticles
+          subtle
+          title={`articles - ${props.pageContext.tag.replace(/\//g, '')}`}
+          recentArticles={posts}
+        />
+      )}
+    </Page>
+  </Layout>
+)
+
+export default TaggedArticles
+
+export const filterJournalsByTags = graphql`
+  query filterJournalsByTags($tag: String) {
+    allMarkdownRemark(filter: { frontmatter: { tags: { regex: $tag } } }) {
+      edges {
+        node {
+          html
+          id
+          frontmatter {
+            path
+            title
+            cover
+            date
+            tags
+          }
+        }
+      }
+    }
+  }
+`
