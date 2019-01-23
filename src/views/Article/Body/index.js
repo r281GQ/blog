00fabcfbeb45c0ce713f77d787f265bbefa1b719
Endre Vegh <endre.vegh@yahoo.com>
2019-01-23 21:55:12 +0000

    refactor Article with query

--- src/views/Article/Body/index.js
+++ src/views/Article/Body/index.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import { Box, Flex, Text } from 'rebass'
+import { Flex, Text } from 'rebass'
 
 import Hyphens from './../../../components/Hyphens'
 
-import ContentTable from './../ContentTable'
+import ArticleDetail from '../../Sidebar/ArticleDetail'
 
 const Body = props => (
   <Flex
     flex="1 1 0"
-    flexDirection={['column', 'column', 'row-reverse']}
-    justifyContent="space-between"
+    flexDirection={[
+      'column-reverse',
+      'column-reverse',
+      'column-reverse',
+      'row',
+    ]}
+    justifyContent="space-around"
   >
-    <ContentTable list={props.links} />
-    <Box css={{ maxWidth: '800px' }} pr={[2, 2, 4, 6]} py={4} pl={[2, 2, 0, 0]}>
-      <Text as={Hyphens} fontFamily="Muli" fontSize={1} lineHeight={1.6}>
-        <div dangerouslySetInnerHTML={{ __html: props.content }} />
-      </Text>
-    </Box>
+    <Text
+      as={Hyphens}
+      css={{ maxWidth: '800px' }}
+      fontFamily="Muli"
+      fontSize={1}
+      lineHeight={1.6}
+    >
+      <div dangerouslySetInnerHTML={{ __html: props.content }} />
+    </Text>
+    <ArticleDetail {...props.details} />
   </Flex>
 )
 
--- src/views/Article/index.js
+++ src/views/Article/index.js
@@ -1,91 +1,54 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import { Box } from 'rebass'
+
+import ArticleContainer from './../../containers/Article'
 
 import Layout from '../../components/Layout'
 
 import Heading from './Heading'
 import Body from './Body'
 
-const content =
-  'Years ago an article came out of LinkedIn about the unified log, a useful architectural pattern for services in a distributed system share state with one another. In the log’s design, services emit state changes into an ordered data structure in which each new record gets a unique ID. Unlike a queue, a log is durable across any number of reads until it’s explicitly truncated.\n'
-
-const links = [
-  {
-    link: 'Redis streams',
-    name: 'Redis streams',
-    subLinks: [
-      { link: 'Versus kafka', name: 'Versus kafka' },
-      {
-        link: 'Configuring Redis for durability',
-        name: 'Configuring Redis for durability',
-      },
-    ],
-  },
-  {
-    link: 'Unified Rocket Rides',
-    name: 'Unified Rocket Rides',
-    subLinks: [
-      { link: 'At-least once design', name: 'At-least once design' },
-      {
-        link: 'The API',
-        name: 'The API',
-      },
-      {
-        link: 'The streamer',
-        name: 'The streamer',
-        subLinks: [
-          {
-            link: 'Log truncation',
-            name: 'Log truncation',
-          },
-        ],
-      },
-
-      { link: 'Consumers & checkpointing', name: 'Consumers & checkpointing' },
-      {
-        link: 'Simulating failure',
-        name: 'Simulating failure',
-      },
-    ],
-  },
-  {
-    link: 'Other considerations',
-    name: 'Other considerations',
-    subLinks: [
-      {
-        link: 'Non-transactional consumers & idempotency',
-        name: 'Non-transactional consumers & idempotency',
-      },
-      {
-        link: 'Versus Postgres logical replication',
-        name: 'Versus Postgres logical replication',
-      },
-      {
-        link: 'Are delivery guarantees absolute?',
-        name: 'Are delivery guarantees absolute?',
-      },
-    ],
-  },
-  {
-    link: 'Log-based architecture',
-    name: 'Log-based architecture',
-  },
-]
-
-const details = {
-  name: `Redis Streams and the Unified Log`,
-  date: `November 8, 2017`,
-  tags: ['desk', 'table', 'pen'],
-  location: 'San Francisco',
-}
-
-const Article = () => (
-  <Layout details={details}>
-    <Box flex="1 1 0">
-      <Heading title="Redis Streams and the Unified Log" />
-      <Body links={links} content={content} />
-    </Box>
-  </Layout>
+const Article = props => (
+  <ArticleContainer {...props}>
+    {renderProps => (
+      <Layout>
+        <Box flex="1 1 0">
+          <Heading title={renderProps.title} />
+          <Body
+            details={{
+              name: renderProps.title,
+              tags: renderProps.tags,
+              date: renderProps.date,
+              location: renderProps.location,
+              links: null,
+              repo: null,
+            }}
+            content={renderProps.content}
+          />
+        </Box>
+      </Layout>
+    )}
+  </ArticleContainer>
 )
+
+export const journalQuery = graphql`
+  query journalQuery($path: String) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        description
+        title
+        path
+        cover
+        date
+        tags
+        src
+        size
+        location
+      }
+    }
+  }
+`
 
 export default Article
