3b901c2bad30d87327fff61f6b4fd89dec1b84a1
Endre Vegh <endre.vegh@yahoo.com>
2019-01-31 21:22:38 +0000

    add links to about

--- src/views/About/index.js
+++ src/views/About/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { StaticQuery, graphql } from 'gatsby'
 import { Box, Flex, Image, Text } from 'rebass'
 
 import { Collapseable } from './../../components/Collapseable'
+import ExternalLink from '../../components/ExternalLink'
 import Hyphens from './../../components/Hyphens'
 
 import profile from './../../images/profile.jpg'
@@ -9,42 +11,80 @@ import profile from './../../images/profile.jpg'
 import { Container } from './styled'
 
 const About = props => (
-  <Box flex="1" as={Container} value={800}>
-    <Text fontSize={7} my={4}>
-      about
-    </Text>
-    <Flex flexDirection={['column', 'row']}>
-      <Flex flex="1" flexDirection="column">
-        <Image src={profile} />
-        <Flex
-          flexDirection={['row', 'column']}
-          justifyContent="space-between"
-          alignItems="baseline"
-          my={[4, 0]}
-        >
-          <Text as={Collapseable} my={4} textAlign="center" collapse={0}>
-            contact me
-          </Text>
-          <Box as={Collapseable} collapse={0}>
-            <Text mb={3}>
-              Here are some links to check out. If you have any question feel
-              free to drop me a message using one of the option below.
-            </Text>
-          </Box>
-          <Text>linkedin</Text>
-          <Text>twitter</Text>
-          <Text>email</Text>
-          <Text>github</Text>
-          <Text>youtube</Text>
-        </Flex>
-      </Flex>
-      <Flex px={3} flex="1" width="100%" flexDirection="column">
-        <Text as={Hyphens} fontFamily="Muli">
-          <div dangerouslySetInnerHTML={{ __html: props.content }} />
+  <StaticQuery
+    query={graphql`
+      query userLinks {
+        site {
+          siteMetadata {
+            userTwitter
+            userLinkedIn
+            userGitHub
+            userEmail
+            userYoutube
+          }
+        }
+      }
+    `}
+    render={result => (
+      <Box flex="1" as={Container} value={800}>
+        <Text fontSize={7} my={4}>
+          about
         </Text>
-      </Flex>
-    </Flex>
-  </Box>
+        <Flex flexDirection={['column', 'row']}>
+          <Flex flex="1" flexDirection="column">
+            <Image src={profile} />
+            <Flex
+              flexDirection={['row', 'column']}
+              justifyContent="space-between"
+              alignItems="baseline"
+              my={[4, 0]}
+            >
+              <Text as={Collapseable} my={4} textAlign="center" collapse={0}>
+                contact me
+              </Text>
+              <Box as={Collapseable} collapse={0}>
+                <Text mb={3}>
+                  Here are some links to check out. If you have any question
+                  feel free to drop me a message using one of the option below.
+                </Text>
+              </Box>
+              <Text>
+                <ExternalLink to={result.site.siteMetadata.userTwitter}>
+                  twitter
+                </ExternalLink>
+              </Text>
+              <Text>
+                <ExternalLink to={result.site.siteMetadata.userLinkedIn}>
+                  linkedin
+                </ExternalLink>
+              </Text>
+              <Text>
+                <ExternalLink to={result.site.siteMetadata.userGitHub}>
+                  github
+                </ExternalLink>
+              </Text>
+              <Text>
+                <ExternalLink to={result.site.siteMetadata.userEmail}>
+                  email
+                </ExternalLink>
+              </Text>
+              <Text>
+                <ExternalLink to={result.site.siteMetadata.userYoutube}>
+                  youtube
+                </ExternalLink>
+              </Text>
+              <Text my={3}>Site</Text>
+            </Flex>
+          </Flex>
+          <Flex px={3} flex="1" width="100%" flexDirection="column">
+            <Text as={Hyphens} fontFamily="Muli">
+              <div dangerouslySetInnerHTML={{ __html: props.content }} />
+            </Text>
+          </Flex>
+        </Flex>
+      </Box>
+    )}
+  />
 )
 
 export default About
