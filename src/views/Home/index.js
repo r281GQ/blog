91557500cb1c86524d817029d481a3e7cdaa254d
Endre Vegh <endre.vegh@yahoo.com>
2019-01-31 21:06:09 +0000

    feed home about from md

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { StaticQuery } from 'gatsby'
 import { Box, Flex, Text } from 'rebass'
 import styled from 'styled-components'
 
@@ -22,23 +23,31 @@ export default props => (
         about
       </Text>
       <Text fontFamily="Muli">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultrices
-        lorem ac nunc porta interdum. Nam purus erat, porttitor sit amet
-        accumsan at, blandit vel mauris. Praesent nec nisi mauris. Suspendisse
-        nulla dui, sagittis eget venenatis id, elementum nec ante. Integer
-        dignissim sem ipsum, id vehicula justo cursus et. Nulla porttitor massa
-        vitae felis iaculis, at auctor urna accumsan. Nunc semper vulputate arcu
-        a dignissim. In in velit maximus, vulputate libero in, lobortis leo.
-        Quisque vel leo eget elit ornare aliquam. Praesent sed lectus vitae ante
-        blandit molestie. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Praesent aliquam commodo
-        consequat. Cras tempor convallis dolor. Praesent egestas volutpat est,
-        id lacinia diam elementum venenatis. Curabitur pellentesque justo eget
-        nibh posuere, eget iaculis eros mattis. Curabitur commodo egestas felis
-        sed ornare. Proin sollicitudin vehicula accumsan. Maecenas tempor porta
-        elit ac pretium. Vivamus in arcu sed felis egestas egestas. Nulla varius
-        lacus et lacus pretium, a faucibus odio fermentum. Nam convallis, diam
-        eu commodo porttitor, sapien augue dignissim neque,
+        <StaticQuery
+          query={graphql`
+            query aboutHome {
+              allMarkdownRemark(
+                filter: { frontmatter: { category: { eq: "about-home" } } }
+              ) {
+                edges {
+                  node {
+                    html
+                    frontmatter {
+                      category
+                    }
+                  }
+                }
+              }
+            }
+          `}
+          render={result => (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: result.allMarkdownRemark.edges[0].node.html,
+              }}
+            />
+          )}
+        />
       </Text>
     </Box>
     <RecentArticles
