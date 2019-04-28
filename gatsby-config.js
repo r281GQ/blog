1d21c1e098ea2af0ec9b0af82f9719873d3e7d02
Endre Vegh <endre.vegh@yahoo.com>
2019-04-28 18:09:09 +0100

    remove option from ga plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,14 +23,6 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-133645004-1',
-        // Puts tracking script in the head instead of the body
-        head: false,
-        // Setting this parameter is optional
-        anonymize: true,
-        // Setting this parameter is also optional
-        respectDNT: true,
-        sampleRate: 5,
-        siteSpeedSampleRate: 10,
       },
     },
     {
